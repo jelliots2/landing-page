a17b9a6f08f176807bfd5d4dadc1a046a1766a83
jelliots <jelliot.stocker@gmail.com>
2025-01-21 12:42:38 -0500

    text

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -2,6 +2,7 @@
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen">
     <h2 class="text-4xl mb-8">Elliot Stocker</h2>
+    <p class="gray-100 text-1xl mb-8">Music site coming soon</p>
     <div class="space-x-4">
       <button @click="goToSubdomain1" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Music</button>
       <button @click="goToSubdomain2" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Portfolio</button>
@@ -14,7 +15,7 @@ export default {
   name: 'LandingPage',
   methods: {
     goToSubdomain1() {
-      window.location.href = 'https://elliotstocker.net/music';
+      window.location.href = '';
     },
     goToSubdomain2() {
       window.location.href = 'https://portfolio.elliotstocker.net';
